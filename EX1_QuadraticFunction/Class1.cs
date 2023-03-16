8a2f6dc1cda7e6599fe00bcd8f3e8c31aad495c7
nagdim <nagorniy.dmitry@yandex.ru>
2023-03-16 09:01:42 +0300

    Add project files.

---
+++ EX1_QuadraticFunction/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1_QuadraticFunction
+{
+    public class Class1
+    {
+    }
+}
