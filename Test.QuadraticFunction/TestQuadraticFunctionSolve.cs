692f36f5359594515513359846f3e3c92db00814
nagdim <nagorniy.dmitry@yandex.ru>
2023-03-16 09:25:09 +0300

    fix name TestQuadraticFunctionSolve

--- Test.QuadraticFunction/TestQuadraticFunctionSolve.cs
+++ Test.QuadraticFunction/TestQuadraticFunctionSolve.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EX1_QuadraticFunction;
+﻿using EX1_QuadraticFunction;
 using Xunit;
 
 namespace TestQuadraticFunction
