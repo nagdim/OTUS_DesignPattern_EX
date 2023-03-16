957d6729f248c1e304b16478ac2e9bc8a346da9b
nagdim <nagorniy.dmitry@yandex.ru>
2023-03-16 09:23:12 +0300

    Create Projects EX_1

---
+++ EX1_QuadraticFunction/QuadraticFunction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EX1_QuadraticFunction
+{
+    public static class QuadraticFunction
+    {
+        public static double[] Solve(double v1, double v2, double v3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test.QuadraticFunction/TestQuadraticFunctionSolve.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EX1_QuadraticFunction;
+using Xunit;
+
+namespace TestQuadraticFunction
+{
+    public class TestQuadraticFunctionSolve
+    {
+
+        /// <summary>
+        /// Написать тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
+        /// </summary>
+        [Fact]
+        public void Solve_NoRootOfTheEquation()
+        {
+            double[] res = QuadraticFunction.Solve(1.0, 0.0, 1.0);
+
+            Assert.Empty(res);
+        }
+    }
+}
