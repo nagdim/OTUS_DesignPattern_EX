ed468c4779f5ff52fcf6efdcb28680f7d0676ef5
nagdim <nagorniy.dmitry@yandex.ru>
2023-03-17 13:00:34 +0300

    test release

--- EX1_QuadraticFunction/QuadraticFunction.cs
+++ EX1_QuadraticFunction/QuadraticFunction.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Linq;
 
 namespace EX1_QuadraticFunction
 {
     public static class QuadraticFunction
     {
-        public static double[] Solve(double v1, double v2, double v3)
+        private const int c_delimeter = 5;
+        private static readonly Predicate<double>[] IndicateSpecifiedValueFunctions = new Predicate<double>[] { double.IsNaN, double.IsNegativeInfinity, double.IsPositiveInfinity, double.IsInfinity };
+
+        public static double MinDoubleLimit = 0.000001;//1E-16;
+
+
+        public static double[] Solve(double a, double b, double c)
         {
-            throw new NotImplementedException();
+            ValidateArgument(nameof(a), a);
+            ValidateArgument(nameof(b), b);
+            ValidateArgument(nameof(c), c);
+
+            if (RoundLimit(a) == 0.0)
+                throw new ArgumentException($"Argument {nameof(a)} cannot equal to zero.");
+
+            var d = RoundLimit(b * b - 4 * a * c);
+
+            ValidateArgument("discriminant", d);
+
+            if (d < 0.0)
+                return Array.Empty<double>();
+
+            var sqrt_d = 0.0;
+
+            if (d > 0.0)
+                sqrt_d = Math.Sqrt(d);
+
+            var x1 = RoundLimit((-b + sqrt_d) / (2 * a));
+            var x2 = RoundLimit((-b - sqrt_d) / (2 * a));
+
+            return new double[] { Math.Round(x1, c_delimeter), Math.Round(x2, c_delimeter) };
+        }
+
+        private static double RoundLimit(double value)
+        {
+            return Math.Abs(value) <= MinDoubleLimit ? 0.0 : value;
+        }
+
+        private static void ValidateArgument(string argumentName, double value)
+        {
+            foreach (var func in IndicateSpecifiedValueFunctions)
+            {
+                if (func(value))
+                    throw new NotSupportedException($"Argument {argumentName} not supporterd to {value}");
+            }
         }
     }
 }
--- Test.QuadraticFunction/TestQuadraticFunctionSolve.cs
+++ Test.QuadraticFunction/TestQuadraticFunctionSolve.cs
@@ -1,8 +1,10 @@
 ﻿using EX1_QuadraticFunction;
+using System;
 using Xunit;
 
 namespace TestQuadraticFunction
 {
+
     public class TestQuadraticFunctionSolve
     {
 
@@ -10,11 +12,62 @@ namespace TestQuadraticFunction
         /// Написать тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
         /// </summary>
         [Fact]
-        public void Solve_NoRootOfTheEquation()
+        public void Solve_1_NoRoot()
         {
             double[] res = QuadraticFunction.Solve(1.0, 0.0, 1.0);
 
             Assert.Empty(res);
+        }
+
+        /// <summary>
+        /// Написать тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)
+        /// </summary>
+        [Fact]
+        public void Solve_2_TwoRootAnd()
+        {
+            double[] res = QuadraticFunction.Solve(1.0, 0.0, -1.0);
+
+            Assert.Equal(2, res.Length);
+            Assert.Equal(res, new double[] { 1.0, -1.0 });
+        }
+
+        /// <summary>
+        /// Написать тест, который проверяет, что для уравнения x^2+2x+1 = 0 есть один корень кратности 2 (x1= x2 = -1).
+        /// </summary>
+        [Fact]
+        public void Solve_3_TwoSameRoot()
+        {
+            double[] res = QuadraticFunction.Solve(1.0, 2.0, 1.0);
+
+            Assert.Equal(2, res.Length);
+            Assert.Equal(res, new double[] { -1.0, -1.0 });
+        }
+
+        [Fact]
+        public void Solve_4_FirstArgumentEqualZeroOrTooSmall()
+        {
+            Assert.Throws<ArgumentException>(() => QuadraticFunction.Solve(0.0, 2.0, 1.0));
+            Assert.Throws<ArgumentException>(() => QuadraticFunction.Solve(QuadraticFunction.MinDoubleLimit, 2.0, 1.0));
+        }
+
+        [Fact]
+        public void Solve_5_DiscriminantLessThanMinDoubleLimit()
+        {
+            double[] res = QuadraticFunction.Solve(1.0, 2.0 + QuadraticFunction.MinDoubleLimit, 1.0 + QuadraticFunction.MinDoubleLimit);
+
+            Assert.Equal(2, res.Length);
+            Assert.Equal(res, new double[] { -1.0, -1.0 });
+        }
+
+        [Fact]
+        public void Solve_5_UndefinedArguments()
+        {
+            foreach (var value in new double[] { double.NaN, double.NegativeInfinity, double.PositiveInfinity, double.MaxValue })
+            {
+                Assert.Throws<NotSupportedException>(() => QuadraticFunction.Solve(value, 1, 1));
+                Assert.Throws<NotSupportedException>(() => QuadraticFunction.Solve(1, value, 1));
+                Assert.Throws<NotSupportedException>(() => QuadraticFunction.Solve(1, 1, value));
+            }
         }
     }
 }
