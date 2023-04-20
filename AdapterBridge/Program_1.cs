ede64bdfe46fd3e83eb613541cd816b4a9ddaf45
nagdim <nagorniy.dmitry@yandex.ru>
2023-04-20 17:45:08 +0300

    Homework #12 Adapter & Bridge

--- AdapterBridge/Program_1.cs
+++ AdapterBridge/Program_1.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Xml;
+﻿using System.Xml;
 
 namespace AdapterBridge
 {
@@ -7,13 +6,6 @@ namespace AdapterBridge
     {
         private const string c_fileRead = "f0";
         private const string c_fileWrite = "f1";
-
-        //private readonly IReadWriteCreator m_creator;
-
-        //public Program_1(IReadWriteCreator creator)
-        //{
-        //    m_creator = creator;
-        //}
 
         public void Run(IReadWriteCreator creator)
         {
@@ -26,76 +18,6 @@ namespace AdapterBridge
             var writer = creator.WriteTo(c_fileWrite);
 
             writer.Write("R", data_R);
-        }
-    }
-
-    public class Program_3 : IProgramRunner
-    {
-        private readonly IProgramRunner m_program_1;
-        private readonly IProgramRunner m_program_2;
-        public Program_3(IProgramRunner program_1, IProgramRunner program_2)
-        {
-            m_program_1 = program_1;
-            m_program_2 = program_2;
-        }
-
-        public void Run(IReadWriteCreator creator)
-        {
-            creator = new ProgramAdapter(creator);
-
-            m_program_2.Run(creator);
-            m_program_1.Run(creator); 
-        }
-    }
-
-
-    public class ProgramAdapter : IWriter, IReader, IReadWriteCreator
-    {
-        private readonly Dictionary<string, object> m_storage;
-        private readonly IReadWriteCreator m_creator;
-        private IWriter m_writer;
-        private IReader m_reader;
-
-        public ProgramAdapter(IReadWriteCreator creator)
-        {
-            m_storage = new Dictionary<string, object>();
-            m_creator = creator;
-        }
-
-        T IReader.Read<T>(string key)
-        {
-            if (m_storage.Count == 0)
-            {
-                if (m_reader == null)
-                    throw new System.Exception($"First you need to call {nameof(ReadFrom)}.");
-
-                return (T)m_reader.Read<T>(key);
-            }
-
-            return (T)m_storage[key];
-        }
-
-        void IWriter.Write<T>(string key, T data)
-        {
-            if (m_writer == null)
-                throw new System.Exception($"First you need to call {nameof(WriteTo)}.");
-
-            m_writer.Write(key, data);
-            m_storage[key] = data;
-        }
-
-        public IReader ReadFrom(string path)
-        {
-            if (m_storage.Count == 0)
-                m_reader = m_creator.ReadFrom(path);
-
-            return this;
-        }
-
-        public IWriter WriteTo(string path)
-        {
-            m_writer = m_creator.WriteTo(path);
-            return this;
         }
     }
 }
